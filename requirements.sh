34828852b71a9b21ae904c10947d975183922cb1
vilesport <89498002+vilesport@users.noreply.github.com>
2024-12-18 16:39:48 +0700

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,4 @@
-sudo apt update
+sudo apt update ;
+sudo apt install -y qemu-system ;
+sudo apt install -y qemu-user-static ;
 sudo apt install -y build-essential gcc make binutils flex bison bc libelf-dev
