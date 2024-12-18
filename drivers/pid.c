f4a8279a5bd853ece46bb5d8f7abf01efe30580f
vilesport <89498002+vilesport@users.noreply.github.com>
2024-12-18 16:19:20 +0700

    Update pid.c, makefile

--- drivers/pid.c
+++ drivers/pid.c
@@ -11,8 +11,13 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/vmalloc.h>
-#include <linux/kernel.h>
 #include <asm/uaccess.h>
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0) 
+#define HAVE_PROC_OPS 
+#define state __state
+#endif 
 
 #define BUFFER_SIZE 0x100
 #define PROC_NAME "pid"
@@ -26,17 +31,24 @@ static long l_pid;
 static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
 static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos);
 
+#ifdef HAVE_PROC_OPS
 static struct proc_ops proc_ops = {
         .proc_read = proc_read, 
         .proc_write = proc_write
 };
+#else
+static struct file_operations proc_ops = {
+        .owner = THIS_MODULE,
+        .read = proc_read,
+        .write = proc_write,
+};
+#endif
 
 /* This function is called when the module is loaded. */
 static int proc_init(void)
 {
         // creates the /proc/procfs entry
         proc_create(PROC_NAME, 0666, NULL, &proc_ops);
-
         printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
 	return 0;
@@ -64,27 +76,26 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
         int rv = 0;
         char buffer[BUFFER_SIZE];
         static int completed = 0;
-        struct task_struct *tsk = NULL;
+        struct pid *target = NULL;
+        struct task_struct *task = NULL;
+        
 
         if (completed) {
                 completed = 0;
                 return 0;
         }
         
-        struct pid * target = find_vpid(l_pid);
-        if(target == NULL)
-                tsk = NULL;
-        else
-                tsk = pid_task(target, PIDTYPE_PID);
+        target = find_vpid(l_pid);
+        task = pid_task(target, PIDTYPE_PID);
 
-        if(tsk == NULL)
-                scnprintf(buffer, sizeof(buffer), "PID %ld not found\n", l_pid);
-        else
-                scnprintf(buffer, sizeof(buffer), "command = [%s] pid = [%d] state = [%d]\n", &tsk->comm[0], tsk->pid, tsk->__state);
-        
-        rv = strlen(buffer);
+        if(task == NULL) {
+                printk(KERN_INFO "PID %ld not found\n", l_pid);
+                return 0;
+        }
 
         completed = 1;
+
+        rv = snprintf(buffer, BUFFER_SIZE, "command = [%s], pid = [%d], state = [%d]\n", task->comm, task->pid, task->state);
 
         if (copy_to_user(usr_buf, buffer, rv))
         {
@@ -101,9 +112,8 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
 static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos)
 {
         char *k_mem = NULL;
-        long pid_res = 0;
         // allocate kernel memory
-        k_mem = kmalloc(count, GFP_KERNEL);
+        k_mem = kmalloc(count + 1, GFP_KERNEL);
 
         /* copies user space usr_buf to kernel buffer */
         if (copy_from_user(k_mem, usr_buf, count)) {
@@ -111,10 +121,10 @@ static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t 
                 return -1;
         }
 
-        sscanf(k_mem, "%ld", &pid_res);
-        kfree(k_mem);
+        k_mem[count] = '\0'; // Ensure null termination
+        sscanf(k_mem, "%ld", &l_pid);
         
-        l_pid = pid_res;
+        kfree(k_mem);
         return count;
 }
 
